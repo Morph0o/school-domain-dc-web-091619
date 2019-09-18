ec515a2d3911d50cfc8cceab9ef8ecf61f51ea98
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-18 15:12:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+ attr_accessor :roster
+ 
+  def initialize(name)
+    @roster = {}
+    @school = name
+  end
+  
+  def add_student(name,grade)
+    if roster[grade]
+      roster[grade].push[name]
+  else 
+     roster[grade]= []
+      roster[grade].push[name]
+    end
+  end 
+  
+  def grade(num)
+   return roster[num]
+ end
+  
+end
